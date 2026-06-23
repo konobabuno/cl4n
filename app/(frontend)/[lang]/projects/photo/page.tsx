6eebde2f0a186f1c1c2efa26d1b3e701f8f42dae
Robe Jafet <robejafet@gmail.com>
2026-06-23 16:58:38 -0600

    aspect fix

--- app/(frontend)/[lang]/projects/photo/page.tsx
+++ app/(frontend)/[lang]/projects/photo/page.tsx
@@ -3,8 +3,6 @@ import RenderPhotos from "@/components/RenderPhotos";
 
 export default async function PhotoPage() {
     const { photos } = await fetchPhotoPage(0, 9);
-    console.log(photos);
-
     
     return (
        <RenderPhotos photos={photos} />
